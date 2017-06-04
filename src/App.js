2a14928c16bfb3a4a99aff05d04067202c3ba632
Abhishek Warokar <abhishek11@live.in>
2017-06-04 21:22:11 +0530

    :zap: editor test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,63 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+// TODO: MOVE EDITOR TO SEPARATE COMPONENT
+import React, {Component} from 'react';
+import CodeMirror from 'react-codemirror';
 import './App.css';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/paraiso-dark.css';
+
+require('codemirror/mode/javascript/javascript');
+require('codemirror/mode/htmlmixed/htmlmixed');
+require('codemirror/mode/css/css');
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jsCode: "// JS is life",
+      htmlCode: "<!-- <body></body> -->",
+      cssCode: "/* CSS styles here */"
+    };
+
+  }
+
+  getOutputSource = () => {
+    return '<html><head><style>body{border:0;padding:0}' + this.state.cssCode + '</style></head><body>' + this.state.htmlCode + '<script>' + this.state.jsCode + '</script></body></html>'
+  }
+
+  updateHtmlCode = (newCode) => {
+    this.setState({htmlCode: newCode});
+  }
+
+  updateCssCode = (newCode) => {
+    this.setState({cssCode: newCode});
+  }
+
+  updateJsCode = (newCode) => {
+    this.setState({jsCode: newCode});
+  }
+
+  getOptions = (modeName) => {
+    return {lineNumbers: true, lineWrapping: true, theme: 'paraiso-dark', mode: modeName}
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <CodeMirror
+          value={this.state.htmlCode}
+          onChange={this.updateHtmlCode}
+          options={this.getOptions('htmlmixed')}
+          className="Editor"/>
+        <CodeMirror
+          value={this.state.cssCode}
+          onChange={this.updateCssCode}
+          options={this.getOptions('css')}
+          className="Editor"/>
+        <CodeMirror
+          value={this.state.jsCode}
+          onChange={this.updateJsCode}
+          options={this.getOptions('javascript')}
+          className="Editor"/>
       </div>
     );
   }
